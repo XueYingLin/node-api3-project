c67b3f40442073ebbee3fadfcf1057886d7f9744
Nikki Lin <nikkiylin@gmail.com>
2020-04-14 16:26:16 -0700

    index.js ready

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code away!
+const express = require("express")
+const users = require("./users/userDb")
+const userRouter = require("./users/userRouter")
+
+const server = expres()
+const port = 4000
+
+server.use(express.json())
+server.use("./users", userRouter)
+
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`)
+})
